3ea78c3a2dac6a2d5524e2104c86f7db3c4c359b
animucki <b.animucki@gmail.com>
2026-05-10 10:25:35 +0200

    Basic subscriber: parse XML

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # turbo-octo-tribble
 Today's mission: track down one of GVB's bidirectional Siemens Combino trams
+
+Initialize after cloning with:
+
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
--- basicsubscriber.py
+++ basicsubscriber.py
@@ -1,6 +1,9 @@
 from gzip import GzipFile
 from io import BytesIO
+from xml.etree import ElementTree
 import zmq
+
+namespace = "http://bison.connekt.nl/tmi8/kv6/msg"
 
 context = zmq.Context()
 
@@ -14,7 +17,13 @@ while True:
     contents = b"".join(multipart[1:])
     try:
         contents = GzipFile("", "r", 0, BytesIO(contents)).read()
-        print("GZIP", address, contents)
+        # print("GZIP", address, contents)
+        tree = ElementTree.fromstring(contents)
+        for kv6 in tree.findall(f"{{{namespace}}}KV6posinfo"):
+            for msg in kv6:
+                vehicle = msg.findtext(f"{{{namespace}}}vehiclenumber")
+                msg_type = msg.tag.split("}")[1]  # strips namespace from INIT, ONROUTE etc.
+                print(msg_type, vehicle)
     except:
         print("NOT", address, contents)
         raise
